ca7f9050bf3f048ded6c6c16bf8bbabef3ea7ca0
KhanhVy0910 <phamthikhanhvi.ts@gmail.com>
2026-04-16 00:24:48 +0700

    Add print method to Cart

--- src/hust/soict/dsai/aims/cart/Cart.java
+++ src/hust/soict/dsai/aims/cart/Cart.java
@@ -86,4 +86,17 @@ public class Cart {
     public int getQtyOrdered(){
         return qtyOrdered;
     }
+    public void print() {
+        System.out.println("***********************CART***********************");
+        System.out.println("Ordered Items:");
+        float totalCost = 0;
+        
+        for (int i = 0; i < qtyOrdered; i++) {
+            System.out.println((i + 1) + ". " + itemsOrdered[i].toString());
+            totalCost += itemsOrdered[i].getCost();
+        }
+        
+        System.out.println("Total cost: " + totalCost + " $");
+        System.out.println("***************************************************");
+    }
 }
