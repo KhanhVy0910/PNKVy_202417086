588b63f12846d037ab5a5dd02752e91b05d4afce
KhanhVy0910 <phamthikhanhvi.ts@gmail.com>
2026-04-16 00:02:12 +0700

    Add TestPassingParameter adn answers

--- src/hust/soict/dsai/test/disc/TestPassingParameter.java
+++ src/hust/soict/dsai/test/disc/TestPassingParameter.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package hust.soict.dsai.test.disc;
 
-/**
- *
- * @author ADMIN
- */
+package hust.soict.dsai.test.disc;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+
 public class TestPassingParameter {
-    
+
+    public static void main(String[] args) {
+        // TODO - Auto-genarated method stub
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+
+        swap(jungleDVD, cinderellaDVD);
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+
+        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+    }
+
+    public static void swap(Object o1, Object o2) {
+        Object tmp = o1;
+        o1 = o2;
+        o2 = tmp;
+    }
+
+    public static void changeTitle(DigitalVideoDisc dvd, String title) {
+        String oldTitle = dvd.getTitle();
+        dvd.setTitle(title);
+        dvd = new DigitalVideoDisc(oldTitle);
+    }
 }
