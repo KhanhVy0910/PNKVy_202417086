ecc6aeedb5b88de031c7e4af1884c294072d3fd5
KhanhVy0910 <phamthikhanhvi.ts@gmail.com>
2026-04-18 14:25:25 +0700

    Add search cart feature

---
+++ AimProjects/src/hust/soict/dsai/aims/store/Store.java
@@ -0,0 +1,48 @@
+package hust.soict.dsai.aims.store;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+
+
+public class Store {
+    public static final int MAX_ITEMS_IN_STORE = 100;
+    private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[MAX_ITEMS_IN_STORE];
+    private int itemCount = 0;
+    
+    public void addDVD(DigitalVideoDisc dvd) {
+        if (itemCount < MAX_ITEMS_IN_STORE) {
+            itemsInStore[itemCount] = dvd;
+            itemCount++;
+            System.out.println("Added to store: " + dvd.getTitle());
+        } 
+        else {
+            System.out.println("Store is full!");
+        }
+    }
+    
+    public void removeDVD(DigitalVideoDisc dvd) {
+        boolean found = false;
+        for (int i = 0; i < itemCount; i++) {
+            if (itemsInStore[i].equals(dvd)) {
+                // Shift elements
+                for (int j = i; j < itemCount - 1; j++) {
+                    itemsInStore[j] = itemsInStore[j + 1];
+                }
+                itemsInStore[itemCount - 1] = null;
+                itemCount--;
+                found = true;
+                System.out.println("Removed from store: " + dvd.getTitle());
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("DVD not found in store!");
+        }
+    }
+    
+    public void displayStore() {
+        System.out.println("*** STORE INVENTORY ***");
+        for (int i = 0; i < itemCount; i++) {
+            System.out.println((i + 1) + ". " + itemsInStore[i].toString());
+        }
+        System.out.println("***********************");
+    }
+}
