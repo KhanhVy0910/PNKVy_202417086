4219632fcb6d003353d738211fc8b46ebfddc0d0
KhanhVy0910 <phamthikhanhvi.ts@gmail.com>
2026-04-18 13:05:27 +0700

    Add search method to Cart

--- src/hust/soict/dsai/aims/disc/DigitalVideoDisc.java
+++ src/hust/soict/dsai/aims/disc/DigitalVideoDisc.java
@@ -74,4 +74,7 @@ public class DigitalVideoDisc {
         return "DVD - " + title + " - " + category + " - " + director + 
                " - " + length + ": " + cost + " $";
     }
+    public boolean isMatch(String title) {
+        return this.title.toLowerCase().contains(title.toLowerCase());
+    }
 }
